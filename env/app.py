997a0e289262957335be099eef06a3b11732f225
iFrancoPantoja <fpantoja@uninorte.edu.co>
2022-09-20 23:51:55 -0500

    Initial commit

---
+++ env/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/comprar')
+def comprar():
+    return render_template('comprar-tiquetes.html')
